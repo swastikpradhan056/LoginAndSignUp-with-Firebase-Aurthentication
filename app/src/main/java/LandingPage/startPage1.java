44a5ab2e7aa9f4f02df1419337c2163b2bf71127
swastikpradhan056 <swastikpradhan056@gmail.com>
2024-05-16 08:19:22 +0545

    Second commit

---
+++ app/src/main/java/LandingPage/startPage1.java
@@ -0,0 +1,40 @@
+package LandingPage;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.babybuy.R;
+
+public class startPage1 extends AppCompatActivity {
+    Button btn1;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_start_page1);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        btn1 = findViewById(R.id.startPage1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(startPage1.this, startPage2.class));
+            }
+        });
+    }
+}
